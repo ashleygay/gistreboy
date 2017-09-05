a09c7f469913cf814853433d6f028d04e247f687
Pierre Chavanne <chavan_p@epita.fr>
2017-09-05 17:52:18 +0200

    proc: Add base class for 8b adn 16b registers

--- include/processor/registers.hpp
+++ include/processor/registers.hpp
@@ -1,13 +1,20 @@
 #pragma once
 
-class FlagRegister {
+class Register {
+	public:
+		unsigned char value;
+};
+
+class DRegister {
+	public:
+		unsigned short value;
+};
+
+class FlagRegister : private Register {
 	public:
 		FlagRegister() { value = 0; }
 		enum Flag { ZERO, SUBTRACT, HALFCARRY, CARRY };
 		void setFlag(FlagRegister::Flag flag);
 		void unsetFlag(FlagRegister::Flag flag);
 		bool getFlag(FlagRegister::Flag flag);
-
-	private:
-		char value;
 };
