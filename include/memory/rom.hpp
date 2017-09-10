c23396e1d98c124c0939f1cf3cb8407c0557bbba
Corentin Gay <corentin.gay@epita.fr>
2017-09-10 22:14:58 +0200

    mem: adds memory accessor class that will resolve any memory access

---
+++ include/memory/rom.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+enum CatridgeType {};
+
+
+class Cartidge {
+
+public :
+  Cartidge(const std::string file_name);
+  
+  void set_types();
+  void set_ram();
+
+private :
+
+  /* the actual assembly of the cartidge */
+  std::vector<uint8_t> rom;
+
+  /* a program inside the gameboy that boots the machine
+     and jumps to the location of the animated sequence
+     inside the rom */
+  std::vector<uint8_t> boot_rom;
+
+  /* the type of the cartidge see manual */
+  int type;
+
+  /* the MBC of the cartidge, chip that will extend address space,
+     no MBC is a simple 32kb cartidge like Tetris*/
+  int mbc;
+
+  /* the internal ram of the cartidge */
+  std::vector<uint8_t> ram;
+
+  int rom_size;
+  int ram_size;
+
+  uint8_t rom_bank_number;
+  uint8_t ram_bank_number;
+
+  bool has_ram = false;
+  bool has_battery = false;
+  bool has_timer = false;
+  bool has_rumble = false;
+
+  bool ram_enable = false;
+
+};
