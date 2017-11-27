2c267a6635bc439a33bcc39e640d0a34f66d3342
Corentin Gay <corentin.gay@epita.fr>
2017-11-27 11:57:22 +0100

    gui: fixing gui on Ubuntu

--- include/gui/gui_setup.hpp
+++ include/gui/gui_setup.hpp
@@ -22,9 +22,7 @@ using callback_ptr = void (*)(GtkWidget*, gpointer);
 // XMacro to define all the buttons from the interface.
 // menuBar or subMenu to choose where the button will be added
 #define MENU_SETUP \
-	X("Run", run_button_callback, menuBar, NULL) \
-	X("Stop", stop_callback, menuBar, new Helper(window))\
+	X("Run", run_button_callback, subMenu, NULL) \
+	X("Stop", stop_callback, subMenu, new Helper(window))\
 	X("Open image file", open_button_callback, subMenu,\
-		new OpenButtonHelper(window, {{"access::can-read", "TRUE"}}))\
-	X("Save state", NYI, subMenu, NULL) \
-	X("Load state", NYI, subMenu, NULL)
+		new OpenButtonHelper(window, {{"access::can-read", "TRUE"}}))
