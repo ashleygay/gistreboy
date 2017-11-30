006a68a3e684c6ea2d45db0192518c77e91319b1
Corentin Gay <corentin.gay@epita.fr>
2017-11-30 18:41:38 +0100

    registers: remove dead code (previous version of interrupts)

--- include/processor/registers.hpp
+++ include/processor/registers.hpp
@@ -16,21 +16,3 @@ class FlagRegister : private Register {
 		void unsetFlag(FlagRegister::Flag flag);
 		bool getFlag(FlagRegister::Flag flag);
 };
-
-class InterruptRegister : private Register
-{
-	// This class represents InterruptEnable or InterruptFlag
-	// It represents either that the interrupt is enabled
-	// or that there is an interrupt pending
-
-	// Bit 4 : Joypad
-	// Bit 3 : Serial
-	// Bit 2 : Timer
-	// Bit 1 : LCD STAT
-	// Bit 0 : VBLANK
-	public:
-		InterruptRegister() {value = 0x00;}
-		enum Interrupt { VBLANK, LCD_STATUS, TIMER, SERIAL, JOYPAD };
-		void setInterrupt(InterruptRegister::Interrupt interrupt);
-		void resetInterrupt(InterruptRegister::Interrupt interrupt);
-};
--- src/processor/registers.cpp
+++ src/processor/registers.cpp
@@ -52,14 +52,3 @@ bool FlagRegister::getFlag(FlagRegister::Flag flag)
 	}
 	return false;
 }
-
-
-void InterruptRegister::setInterrupt(InterruptRegister::Interrupt interrupt)
-{
-	value |= (1 << interrupt);
-}
-		
-void InterruptRegister::resetInterrupt(InterruptRegister::Interrupt interrupt)
-{
-	value ^= (1 << interrupt);
-}
